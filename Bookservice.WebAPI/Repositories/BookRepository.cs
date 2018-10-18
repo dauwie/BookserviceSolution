9ddaecce27b36dff22cf18951ac43680b325521b
Joachim Francois <joachim.francois@howest.be>
2018-10-18 13:35:11 +0200

    Add BookRepository with List of Book and List of BookBasic

---
+++ Bookservice.WebAPI/Repositories/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using Bookservice.WebAPI.Data;
+using Bookservice.WebAPI.DTO;
+using Bookservice.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookservice.WebAPI.Repositories
+{
+    public class BookRepository
+    {
+        private BookServiceContext bookServiceContext;
+
+        public BookRepository(BookServiceContext context)
+        {
+            bookServiceContext = context;
+        }
+
+        public List<Book> List()
+        {
+            return bookServiceContext.Books.ToList();
+        }
+
+        public List<BookBasic> ListBasic()
+        {
+            return bookServiceContext.Books.Select(
+                b => new BookBasic
+                {
+                    Id = b.Id,
+                    Title = b.Title
+                }).ToList();
+        }
+    }
+}
