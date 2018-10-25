8516b4750d8579b58277dba9c4a8cbe9a0b3b4f9
Joachim Francois <joachim.francois@howest.be>
2018-10-25 09:57:59 +0200

    Update PublisherRepository with Update method

--- Bookservice.WebAPI/Repositories/PublisherRepository.cs
+++ Bookservice.WebAPI/Repositories/PublisherRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Bookservice.WebAPI.Data;
 using Bookservice.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,33 @@ namespace Bookservice.WebAPI.Repositories
         public Publisher GetById(int id)
         {
             return bookServiceContext.Publishers.FirstOrDefault(p => p.Id == id);
+        }
+
+        public async Task<Publisher> Update(Publisher publisher)
+        {
+            try
+            {
+                bookServiceContext.Entry(publisher).State = EntityState.Modified;
+                await bookServiceContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublisherExists(publisher.Id))
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }                
+            }
+
+            return publisher;
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return bookServiceContext.Publishers.Any(p => p.Id == id);
         }
     }
 }
