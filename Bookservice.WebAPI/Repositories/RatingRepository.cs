9e34c090b95c19a98c60248a2edd39b06436c440
Joachim Francois <joachim.francois@howest.be>
2018-11-07 19:18:14 +0100

    Add RatingRepository

---
+++ Bookservice.WebAPI/Repositories/RatingRepository.cs
@@ -0,0 +1,23 @@
+﻿using Bookservice.WebAPI.Data;
+using Bookservice.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bookservice.WebAPI.Repositories
+{
+    public class RatingRepository : Repository<Rating>
+    {
+        public RatingRepository(BookServiceContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Rating>> GetAllInclusive()
+        {
+            return await GetAll()
+                .Include(r => r.Book)
+                .Include(r => r.Reader)
+                .ToListAsync();
+        }
+    }
+}
