4bb31a37593b56001980afe0061096136e2ac7fe
Joachim Francois <joachim.francois@howest.be>
2018-11-07 19:22:58 +0100

    Add RatingsController

---
+++ Bookservice.WebAPI/Controllers/RatingsController.cs
@@ -0,0 +1,23 @@
+﻿using Bookservice.WebAPI.Models;
+using Bookservice.WebAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Bookservice.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingsController : ControllerCrudBase<Rating, RatingRepository>
+    {
+        public RatingsController(RatingRepository ratingRepository) : base(ratingRepository)
+        {
+        }
+
+        // GET: api/Ratings
+        [HttpGet]
+        public override async Task<IActionResult> Get()
+        {
+            return Ok(await repository.GetAllInclusive());
+        }
+    }
+}
