94c57a5595267facfcb4b2bb9962242f0cfc4061
Joachim Francois <joachim.francois@howest.be>
2018-11-07 19:08:27 +0100

    Add Rating class

---
+++ Bookservice.WebAPI/Models/Rating.cs
@@ -0,0 +1,23 @@
+﻿namespace Bookservice.WebAPI.Models
+{
+    public class Rating : EntityBase
+    {
+        public int ReaderId { get; set; }
+        public Reader Reader { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int Score { get; set; }
+
+        public Rating()
+        {
+        }
+
+        public Rating(int id, int readerId, int bookId, int score)
+        {
+            Id = id;
+            ReaderId = readerId;
+            BookId = bookId;
+            Score = score;
+        }
+    }
+}
