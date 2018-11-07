537043f6e98aa4466fb6c6fc5977b0ee7fa6d0a4
Joachim Francois <joachim.francois@howest.be>
2018-11-07 19:08:37 +0100

    Add Reader class

---
+++ Bookservice.WebAPI/Models/Reader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Bookservice.WebAPI.Models
+{
+    public class Reader : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Rating> Ratings { get; set; }
+
+        public Reader()
+        {
+        }
+
+        public Reader(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
