a47bfbbbaf88b586cc68d4191e5d93c2579a8cc9
Joachim Francois <joachim.francois@howest.be>
2018-11-07 19:20:48 +0100

    Add ReadersController

---
+++ Bookservice.WebAPI/Controllers/ReadersController.cs
@@ -0,0 +1,15 @@
+﻿using Bookservice.WebAPI.Models;
+using Bookservice.WebAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookservice.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadersController : ControllerCrudBase<Reader, ReaderRepository>
+    {
+        public ReadersController(ReaderRepository readerRepository) : base(readerRepository)
+        {
+        }
+    }
+}
